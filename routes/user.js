7dbacecbca730a322d107ab481a5473bd49f3b73
Eiji Hachiya <hachiya_eiji@cyberagent.co.jp>
2014-12-19 19:44:20 +0900

    ちょっと実装

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,15 @@
 'use strict';
+var User = require('../models').User;
 
 exports.login = function (req, res, next) {
 
 };
 
+exports.create = function (req, res, next) {
+
+};
+
+// 多分本当はわけたほうがいいよね.
 exports.debugLogin = function (req, res, next) {
   req.session.user = {
     id: 1
@@ -11,3 +17,16 @@ exports.debugLogin = function (req, res, next) {
   res.status(200).end();
 };
 
+exports.debugCreate = function (req, res, next) {
+  User.create({
+    id: 1,
+    loginId: 'hachi_eiji',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/995846?v=3'
+  }, function (err, user) {
+    if (err) {
+      return next(err);
+    }
+    req.session.user = user;
+    res.status(200).end();
+  });
+};
