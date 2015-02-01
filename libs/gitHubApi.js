6351e2af77ff1c23be8e23adf31d65b4b4ce6163
hachi-eiji <hachiyae@gmail.com>
2015-02-02 02:14:13 +0900

    refactoring
    add test code

--- libs/gitHubApi.js
+++ libs/gitHubApi.js
@@ -1,125 +1,119 @@
 /**
- * @module libs/gitHubApi
+ * @file GitHub API
  */
 'use strict';
-var appConfig = require('./config');
 var https = require('https');
 var querystring = require('querystring');
 
+module.exports = GitHubApi;
+
+function GitHubApi(config) {
+  this.config = config || {
+    host: 'github.com',
+    port: 443
+  };
+}
+
 /**
  * get user via GitHub API.
- * @param {String} code - code
- * @param {Function} cb - callback method
+ * @param {string} code - code
+ * @param {Function} callback - callback method
  */
-exports.getUser = function (code, cb) {
-  this.getAccessToken(code, function (err, accessToken) {
+GitHubApi.prototype.getUser = function(code, callback) {
+  var self = this;
+  self._getAccessToken(code, function(err, accessToken) {
     if (err) {
-      if (cb) {
-        cb(err, null);
-      }
+      return callback(err);
     }
-    getUser(accessToken, cb);
+    return self._getUser(accessToken, callback);
   });
 };
 
 /**
- * get access token via GitHub.
+ * get access token.
  *
- * @param {String} code - code from GitHub request
- * @param {Function} cb - callback method
- * @public
+ * @param {string} code - code
+ * @param {Function} callback - callback method
  */
-exports.getAccessToken = function (code, cb) {
+GitHubApi.prototype._getAccessToken = function(code, callback) {
   if (!code) {
     var error = new Error('code is undefined');
-    if (cb) {
-      cb(error, null);
-    }
+    return callback(error);
   }
+
+  var config = this.config;
   var data = querystring.stringify({
-    client_id: appConfig.gitHubAuth.client_id,
-    client_secret: appConfig.gitHubAuth.secret,
+    client_id: config.client_id,
+    client_secret: config.client_secret,
     code: code
   });
+
   var option = {
-    hostname: 'github.com',
-    port: 443,
-    path: '/login/oauth/access_token',
-    method: 'POST',
-    headers: {
+    hostname: config.host,
+    port: config.port,
+    path: (config.auth && config.auth.path) || '/login/oauth/access_token',
+    method: (config.auth && config.auth.method) || 'POST',
+    hearders: {
       'Content-Length': Buffer.byteLength(data),
       'Accept': 'application/json'
     }
   };
-  var req = https.request(option, function (res) {
+  var req = https.request(option, function(res) {
     res.setEncoding('utf-8');
     if (res.statusCode >= 400) {
-      cb(new Error('status code is ' + res.statusCode));
+      return callback(new Error('status code is ' + res.statusCode));
     }
     var responseData = '';
-    res.on('data', function (chunk) {
+    res.on('data', function(chunk) {
       responseData += chunk;
     });
-    res.on('end', function () {
-      if (cb) {
-        var json = JSON.parse(responseData);
-        if (!json.error) {
-          cb(null, json.access_token);
-        } else {
-          cb(new Error(json.error));
-        }
+    res.on('end', function() {
+      var json = JSON.parse(responseData);
+      if (json.error) {
+        return callback(new Error(json.error));
       }
+      return callback(null, json.access_token);
     });
   });
-  req.on('error', function (err) {
-    if (cb) {
-      cb(err, null);
-    }
+  req.on('error', function(err) {
+    return callback(err, null);
   });
   req.write(data + '\n');
   req.end();
 };
 
-
 /**
- * get user via GitHub API.
- *
- * @param {String} accessToken - access_token
- * @param {Function} cb - callback method
- * @private
+ * request to GitHub
+ * @param {string} accessToken - access token
+ * @param {Function} callback - callback
  */
-function getUser(accessToken, cb) {
+GitHubApi.prototype._getUser = function(accessToken, callback) {
   var req = https.request({
-    hostname: 'api.github.com',
-    port: 443,
-    path: '/user',
-    method: 'GET',
+    hostname: config.host,
+    port: config.port,
+    path: (config.user && config.user.path) || '/user',
+    method: (config.user && config.user.method) || 'GET',
     headers: {
       'Accept': 'application/json',
       'Authorization': 'token ' + accessToken,
       'User-Agent': 'test'
     }
-  }, function (res) {
+  }, function(res) {
     res.setEncoding('utf-8');
     var statusCode = res.statusCode;
     var userData = '';
-    res.on('data', function (chunk) {
+    res.on('data', function(chunk) {
       userData += chunk;
     });
-    res.on('end', function () {
-      if (cb) {
-        var json = JSON.parse(userData);
-        if (statusCode >= 400) {
-          cb(new Error(json.message));
-        } else {
-          cb(null, json);
-        }
+    res.on('end', function() {
+      var json = JSON.parse(userData);
+      if (statusCode >= 400) {
+        return callback(new Error(json.message));
       }
+      return callback(null, json);
     });
-  }).on('error', function (err) {
-    if (cb) {
-      cb(err, null);
-    }
+  }).on('error', function(err) {
+    return callback(err, null);
   });
   req.end();
-}
+};
